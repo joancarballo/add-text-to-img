25d928a165ae3562a1b790d1214d99681b5c2b28
carballo <carballet@gmail.com>
2023-08-12 18:53:48 +0200

    Better code

--- index.js
+++ index.js
@@ -12,35 +12,27 @@ app.use(express.static(__dirname + "/"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// app.get("/", function (req, res) {
-//   res.status(200).sendFile("/Users/carballo/code/add-text-to-img/index.html");
-// });
-
 app.listen(port, function () {
   console.log("========================");
-  console.log("Generating Image");
+  console.log("Generating JPG");
   getImageJPG("El trollsejo rules")
-  getImageGIF("El trollsejo rules y ahora en GIF");
+  console.log("Generating GIF");
+  getImageGIF("El trollsejo rules en GIF");
 });
 
 let getImageJPG = (text) => {
-  // (B) SETTINGS
-  const sFile = "retard.jpg", // source image
-    sSave = "retard-001.jpg", // "save as"
-    sText = text, // text to write
-    sX = 80,
-    sY = 80; // text position
+  const sFile = "retard.jpg" // original image
+  const sSave = "retard-001.jpg" // result image
+  const  sText = text
+  const  sX = 80
+  const  sY = 80
 
-  // (C) LOAD IMAGE + DRAW TEXT
   loadImage(sFile).then((img) => {
-    // (C1) CREATE CANVAS
-    const canvas = createCanvas(img.width, img.height),
-      ctx = canvas.getContext("2d");
+    const canvas = createCanvas(img.width, img.height)
+    const ctx = canvas.getContext("2d");
 
-    // (C2) DRAW IMAGE ONTO CANVAS
     ctx.drawImage(img, 0, 0);
 
-    // (C3) WRITE TEXT ONTO IMAGE
     ctx.font = '55px "Impact Bold"';
     ctx.fillStyle = "white";
     ctx.lineWidth = 2;
@@ -49,44 +41,44 @@ let getImageJPG = (text) => {
         tw = td.width,
         th = td.actualBoundingBoxAscent + td.actualBoundingBoxDescent;
 
-    // (C3) CALCULATE CENTER & WRITE ON CENTER
     let x = Math.floor(((img.naturalWidth - tw) / 2)),
         y = Math.floor(((img.naturalHeight - th) / 2)+220);
     ctx.strokeText(sText, x, y);
     ctx.fillText(sText, x, y);
 
-    // (C4) SAVE
+
     const out = fs.createWriteStream(sSave),
       stream = canvas.createPNGStream();
     stream.pipe(out);
-    out.on("finish", () => console.log("Done"));
+    out.on("finish", () => console.log("JPG Done"));
   });
 };
 
 
-async function getImageGIF(text) {
-  //create a TextOnGif object
+let getImageGIF = async function (text) {
+    const gFile = "retard.gif"; // original GIF
+    const gSave = "retard-001.gif"; // result GIF
+    const gText = text;
+
+
   var gif = new TextOnGif({
-    file_path: "retard.gif",
-    //path to local file, url or Buffer
+    file_path: gFile,
   });
 
-  //get as buffer
   var buffer = await gif.textOnGif({
-    text: text,
+    text: gText,
     get_as_buffer: true,
   });
 
   console.log(buffer);
   gif.on("on frame", (frameIndex) => {
     gif.font_color = "white";
-    // gif.font_size = "40px"
   });
 
-  //write as file
   await gif.textOnGif({
-    text: text,
-    get_as_buffer: false, //set to false to save time
-    write_path: "retard-002.gif",
+    text: gText,
+    get_as_buffer: false,
+    write_path: gSave,
   });
+  console.log("GIF Done");
 };
